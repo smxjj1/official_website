30d28f751c810c8e98d5bea4a025e41e16c7ed89
oliver <16746343+oliverluo001@user.noreply.gitee.com>
2026-04-27 00:20:16 +0800

    feat: 更新icon

--- app/app.vue
+++ app/app.vue
@@ -2,7 +2,7 @@
 // 全局默认设置 - 仅 favicon 和基础配置
 // SEO 设置由各页面通过 useSeo composable 自行管理
 useHead({
-  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/toukie.jpg' }],
   htmlAttrs: {
     lang: 'en',
   },
